39bf0a70f04b750d6c531afa4e96a0a5a274b482
Luix13 <nanaluix@gmail.com>
2026-09-06 23:09:33 +0300

    добавили время ожидания

--- tests/test_form.py
+++ tests/test_form.py
@@ -8,9 +8,12 @@ def test_successful_login(driver):
     """Успешная авторизация"""
     driver.get(BASE_URL)
 
-    username = driver.find_element(By.ID, "username")
-    password = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    wait = WebDriverWait(driver, 15)
+
+    username = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    password = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+
 
     username.send_keys("tomsmith")
     password.send_keys("SuperSecretPassword!")
@@ -26,9 +29,10 @@ def test_unsuccessful_login(driver):
     """Неудачная авторизация (неверный логин)"""
     driver.get(BASE_URL)
 
-    username = driver.find_element(By.ID, "username")
-    password = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    wait = WebDriverWait(driver, 15)
+    username = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    password = wait.until(EC.element_to_be_clickable((By.ID, "password")))
+    login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
 
     # Вводим неверные данные
     username.send_keys("wrong-user")
@@ -38,6 +42,4 @@ def test_unsuccessful_login(driver):
     wait = WebDriverWait(driver, 10)
     flash_msg = wait.until(EC.visibility_of_element_located((By.ID, "flash-messages")))
 
-    # 👇 ИСПРАВЛЕНИЕ ЗДЕСЬ 👇
-    # Сайт пишет "invalid", а не "incorrect". Также там есть символ крестика и перенос строки.
     assert "username is invalid" in flash_msg.text
